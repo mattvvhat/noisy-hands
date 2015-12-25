0cd8f4491523a69fe5cbfbc428c5bb6087f23698
Matt Owen <not.mattowen@gmail.com>
2015-12-25 13:52:57 -0500

    :fallen_leaf

--- app/App.js
+++ app/App.js
@@ -5,7 +5,7 @@ circle_path.fx = function (t) {
 };
 
 circle_path.fy = function (t) {
-  return Math.sin(5*2*Math.PI*t);
+  return Math.sin(2*2*Math.PI*t);
 };
 
 function random(min, max) {
@@ -33,8 +33,8 @@ App.prototype.update = function() {
 App.prototype.draw = function() {
   this.sketchPath(
     circle_path,
-    300,
-    0.005
+    100,
+    0
   );
 };
 
@@ -43,60 +43,8 @@ App.prototype.clear = function() {
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 };
 
-/* ... */
-App.prototype.sketch = function (path, steps, wobble) {
-  this.ctx.save();
-  var p = 100;
-  var w = this.canvas.width-p;
-  var h = this.canvas.height-p;
-  this.ctx.transform(w/2, 0, 0, h/2, w/2+p/2., h/2+p/2.);
-  this.sketchPath(path, steps, wobble);
-  this.ctx.restore();
-};
-
-/* Sketch */
-App.prototype._sketch = function (path, steps, wobble) {
-  // ...
-
-  var h = 1./steps;
-  var xy0 = path(0);
-
-  // ...
-  this.ctx.beginPath();
-  this.ctx.moveTo(
-    xy0[0]+random(-wobble, wobble),
-    xy0[1]+random(-wobble, wobble)
-  );
-
-  // ...
-  for (var i=1; i <= steps; i++) {
-
-    // Get new
-    var xy1 = path(h*(i));
-
-    var vec = [
-      xy1[0] - xy0[0] + random(-wobble, wobble),
-      xy1[1] - xy0[1] + random(-wobble, wobble)
-    ];
-
-    // Pairs
-    var x1 = xy0[0]+vec[0];
-    var y1 = xy0[1]+vec[1];
-
-    xy0 = [x1, y1];
-
-    // context.moveTo(x0, y0);
-    this.ctx.lineTo(x1, y1);
-  }
-
-  // Stroke line
-  this.ctx.strokeStyle = "#000000";
-  this.ctx.lineWidth = 0.0005;
-  this.ctx.stroke();
-};
-
 /**
- *
+ * Sketch Path
  */
 App.prototype.sketchPath = function (path, steps, wobble) {
   this.ctx.save();
@@ -111,8 +59,12 @@ App.prototype.sketchPath = function (path, steps, wobble) {
 App.prototype._sketchPath = function (path, steps, wobble) {
   // ...
 
-  this.h = 1./steps;
+  this.h = 1/steps;
+  this.h = 1.0;
   var xy0 = path.f(0);
+  var first = xy0;
+
+  var wobbles = BoxMuller.nrand(0, wobble);
 
   // ...
   this.ctx.beginPath();
@@ -124,18 +76,19 @@ App.prototype._sketchPath = function (path, steps, wobble) {
   // ...
   for (var i=1; i <= steps; i++) {
     
-    var m = path.df(this.h*i);
+    var m = path.df(i/steps);
+
+    wobbles = BoxMuller.nrand(0, wobble);
 
     // Pairs
-    var x1 = xy0[0] + this.h*m[0] + random(-wobble, wobble);
-    var y1 = xy0[1] + this.h*m[1] + random(-wobble, wobble);
-
-    // console.log(this.h, path.f(h*i), path.df(h*i));
+    var x1 = xy0[0] + 1/steps*m[0] + wobbles[0];
+    var y1 = xy0[1] + 1/steps*m[1] + wobbles[1];
 
     xy0 = [x1, y1];
 
     // context.moveTo(x0, y0);
     this.ctx.lineTo(x1, y1);
+    console.log(x1, y1);
   }
 
   // Stroke line
